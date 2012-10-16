17242fde06442040a36930ab52b65a4b59132bb1
TJ Johnston <cmptrzd1592mc@gmail.com>
2012-10-15 21:50:46 -0600

    Added main code
    Added main code

---
+++ ModDutyDutch/src/com/github/cmptrzd1592/ModDutyDutch/ModDutyDutch.java
@@ -0,0 +1,41 @@
+package com.github.cmptrzd1592.ModDutyDutch;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+
+@SuppressWarnings("unused")
+public class ModDutyDutch extends JavaPlugin {
+
+	@Override
+	public void onEnable(){
+		getLogger().info("onEnable has been invoked!");
+	}
+	
+	@Override
+	public void onDisable(){
+		getLogger().info("onDisable has been invoked!");
+	}
+	
+    public boolean isOnDuty = false;
+	 
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+            if(cmd.getName().equalsIgnoreCase("modduty")){
+             if (sender instanceof Player) {
+            	 Player player = (Player) sender;
+            	 return true;
+             }
+                if(!isOnDuty){
+                	isOnDuty = true;
+                    Bukkit.broadcastMessage(ChatColor.GREEN + sender.getName() + " is nu bezig met tickets.");
+                }else if(isOnDuty){
+                	isOnDuty = false;
+                        Bukkit.broadcastMessage(ChatColor.GREEN + sender.getName() + " is voor nu gestopt met het beantwoorden van tickets.");
+                }
+            } return true;
+    }
+}
